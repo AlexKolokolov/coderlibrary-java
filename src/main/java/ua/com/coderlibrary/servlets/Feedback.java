5f522dd289eb42a6b64a57a081355782242c7e0c
AlexKolokolov <Kolokolov_a@ukr.net>
2016-01-11 19:48:11 +0200

    refactoring made

--- src/main/java/ua/com/coderlibrary/servlets/Feedback.java
+++ src/main/java/ua/com/coderlibrary/servlets/Feedback.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by Administrator on 02.01.2016.
@@ -18,28 +19,34 @@ public class Feedback extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("headerPanel", new HeaderPanel(title, pageId));
-
+        createPageDefaultElements(req);
         req.getRequestDispatcher("feedback.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        sendMessage(req);
+        createPageDefaultElements(req);
+        req.getRequestDispatcher("feedback.jsp").forward(req, resp);
+    }
 
+    private void createPageDefaultElements (HttpServletRequest req) {
+        req.setAttribute("headerPanel", new HeaderPanel(title, pageId));
+    }
+
+    private void sendMessage(HttpServletRequest req) throws UnsupportedEncodingException {
         req.setCharacterEncoding("UTF-8");
-
         String userName = req.getParameter("userName");
         String userEmail = req.getParameter("userEmail");
         String subject = req.getParameter("subject");
         String messageBody = req.getParameter("messageBody");
-
         Mailer.getInstance().sendMessage("kolokolov_a@ukr.net", userEmail, subject,
                 userName + " <" + userEmail + "> : " + messageBody);
-
-        String message = "Спасибо, " + userName + "!<br> Ваше сообщение:<br>\"" + messageBody + "\"<br>отправлено.";
-        req.setAttribute("message", message);
-
-        doGet(req, resp);
+        showSentMessage(req, userName, messageBody);
     }
 
+    private void showSentMessage(HttpServletRequest req, String userName, String messageBody) {
+        String message = "Спасибо, " + userName + "!<br> Ваше сообщение:<br>\"" + messageBody + "\"<br>отправлено.";
+        req.setAttribute("message", message);
+    }
 }
